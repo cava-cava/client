57691ea975036619ba2d6819bb2ff5ed72f4b4f8
Vanroy Julien <julienvanroy@yahoo.fr>
2021-02-09 23:53:01 +0100

    feat: add redux / store

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import reducer from './reducers'
 import './scss/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(reducer)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
