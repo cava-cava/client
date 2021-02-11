261b3ffb3334e46f5ad3cacc08c55466853684a5
Vanroy Julien <julienvanroy@yahoo.fr>
2021-02-11 22:23:52 +0100

    feat: add tips page / store

--- src/store/user/reducer.ts
+++ src/store/user/reducer.ts
@@ -4,7 +4,8 @@ import {SET_NAME, UserActionTypes, userState} from "./types";
 
 export const initialState: userState = {
   data: {
-    name: ''
+    name: '',
+    jwt: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiaWF0IjoxNjEzMDcwODI4LCJleHAiOjE2MTU2NjI4Mjh9.podMQsLLLWnnRRPdbViwUyaF2jZlh5SkTYxiVIljxH4'
   },
   errors: undefined,
   loading: false
@@ -13,7 +14,7 @@ export const initialState: userState = {
 const reducer: Reducer<userState, UserActionTypes> = (state:userState = initialState, action: UserActionTypes) => {
   switch (action.type) {
     case SET_NAME: {
-      return { ...state, data: {name: action.payload} };
+      return { ...state, data: {name: action.payload, jwt: state.data.jwt} };
     }
     default: {
       return state;
