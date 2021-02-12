ff590aaceb11a8c8ed263009a1916ee359e503a8
Vanroy Julien <julienvanroy@yahoo.fr>
2021-02-12 11:58:31 +0100

    feat: add PWA

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux'
 import {Provider} from 'react-redux'
 import configureStore from './configureStore'
 import { createBrowserHistory } from "history";
 import './scss/index.scss';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 const history = createBrowserHistory();
@@ -19,6 +19,11 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
