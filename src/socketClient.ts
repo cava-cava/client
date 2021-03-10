19bbd47408068f866f18a8064b1d0796b7a48370
Vanroy Julien <julienvanroy@yahoo.fr>
2021-03-10 13:50:32 +0100

    fix: connexion with socket client

--- src/socketClient.ts
+++ src/socketClient.ts
@@ -1,3 +1,3 @@
 import io from "socket.io-client";
-const url: string = process.env.NODE_ENV !== 'production' ? 'http://localhost:8080' : window.location.toString();
+const url: string = process.env.NODE_ENV !== 'production' ? 'http://localhost:8080' : window.location.origin.toString();
 export const socket = io(url);
