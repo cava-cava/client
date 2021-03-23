cf8443b1f5ad8e4c722c9194aeb29b7805070908
LeoGeneret <lpgeneret@gmail.com>
2021-03-23 19:16:49 +0100

    resolved merge conflicts

--- src/pages/Labs.tsx
+++ src/pages/Labs.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import TheTimer from '../components/Timer/TheTimer'
+import {Link} from "react-router-dom";
+import Cards from '../components/Cards/Cards'
 
 const Labs = () => {
     return (
         <div className="Labs">
             <h1>LABS</h1>
             <TheTimer />
+            <Cards />
         </div>
     );
 }
