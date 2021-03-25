40aaa80c02bbec0abc0bcb34b3623abc817d218a
Vanroy Julien <julienvanroy@yahoo.fr>
2021-03-25 20:30:31 +0100

    feat: add joker and crasse static

--- src/components/Cards/TheDeck.tsx
+++ src/components/Cards/TheDeck.tsx
@@ -1,20 +1,22 @@
 import React, {FunctionComponent} from 'react';
 import styles from './TheDeck.module.scss'
-import {colors} from "../../mixins/color";
 
-const TheDeck: FunctionComponent = () => {
+type TheDeckProps = {
+    number: number,
+    color?: string,
+    deskClick: () => void
+}
+
+const TheDeck: FunctionComponent<TheDeckProps> = ({number, color, deskClick}) => {
     let cards: any = []
-    for (let i = 0; i < 10; i++) {
-        cards[i] = i
-    }
 
-    const deskClick = () => {
-        console.color(`Tirer une carte`, colors.blue);
+    for (let i = 0; i < number; i++) {
+        cards.push(<div className="card" style={{backgroundColor: color, top: (i * 10)+'px'}} key={i}></div>)
     }
 
     return (
         <div className={styles.TheDeck} onClick={deskClick}>
-            {cards.map(() => <div></div>)}
+            {cards}
         </div>
     )
 }
--- src/pages/Julien.tsx
+++ src/pages/Julien.tsx
@@ -3,13 +3,29 @@ import TheHeader from "../components/Game/Header/TheHeader";
 import styles from './Julien.module.scss'
 import TheProgressBar from "../components/ProgressBar/TheProgressBar";
 import TheDeck from "../components/Cards/TheDeck";
+import {colors} from "../mixins/color";
 
 const Julien = () => {
+    const drawClick = () => {
+        console.color(`Tirer une carte`, colors.blue);
+    }
+
+    const jokerClick = () => {
+        console.color(`joker`, colors.green);
+    }
+
+    const dirtClick = () => {
+        console.color(`crasse`, colors.red);
+    }
     return (
         <div className={styles.Julien}>
             <TheHeader username="JujuOneLove" code="XLRTZ"/>
             <TheProgressBar/>
-            <TheDeck/>
+            <div className={styles.JulienCenter}><TheDeck number={5} deskClick={drawClick}/></div>
+            <div className={styles.JulienBottom}>
+                <TheDeck number={2} color='green' deskClick={jokerClick}/>
+                <TheDeck number={2} color='red' deskClick={dirtClick}/>
+            </div>
         </div>
     );
 }
