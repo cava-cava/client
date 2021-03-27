1d68e9ecb830ff87bf57ba19f3013765d4dd4b56
Vanroy Julien <julienvanroy@yahoo.fr>
2021-03-27 15:18:22 +0100

    style: End game

--- src/pages/End.tsx
+++ src/pages/End.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import styles from './End.module.scss'
 
 const End = () => {
     return (
-        <div className="End">
-            <h1>End Game</h1>
+        <div className={styles.End}>
             <div>
+                <h1>Jujuonelove</h1>
+                <span>1er</span>
+            </div>
+            <div>
+                <h2>Le plus heureux/se</h2>
                 <p>
-                    Tu es le meilleur, gg
+                    Bravo ! Tu as gagné !
+                    Mais ne te mets pas la pression
+                    pour être le meilleur. Penses à toi
+                    et ce que tu as réellement envie !
+                    Comme disait Platon, connais-toi
+                    toi-même pour être heureux.
                 </p>
             </div>
-            <div className="EndButtons">
+            <div className={styles.EndButtons}>
                 <Link to="/rooms">Rejouez</Link>
                 <Link to="/tips">Tips</Link>
             </div>
