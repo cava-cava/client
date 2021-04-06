f6b628fa575ef502328d3d8471698e7e83246167
LeoGeneret <lpgeneret@gmail.com>
2021-04-06 16:47:05 +0200

    wip: add three dependencies + test card

--- src/pages/Labs.tsx
+++ src/pages/Labs.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import Cards from '../components/Cards/Cards'
+import { Canvas } from "@react-three/fiber";
+import styles from "./Labs.module.scss";
+import OrbitControls from "../components/OrbitControls/OrbitControls";
+import { RoundedBox } from "@react-three/drei";
+
+import { Perf } from "r3f-perf";
+import { format } from "node:path";
 
 const Labs = () => {
-    return (
-        <div className="Labs">
-            <h1>LABS</h1>
-            <Cards />
-        </div>
-    );
-}
+  return (
+    <div className={styles.Labs}>
+      <h1>LABS</h1>
+      <Canvas className={styles.canvas}>
+        <Perf />
+        <ambientLight intensity={0.3} />
+        <RoundedBox
+          args={[3, 4, 0.15]}
+          radius={0.1}
+          smoothness={1}
+        >
+          <meshStandardMaterial attach="material" color="lightblue" />
+        </RoundedBox>
+        <OrbitControls />
+      </Canvas>
+    </div>
+  );
+};
 
 export default Labs;
