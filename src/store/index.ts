b7e9df865cf333174737dd88e8da912b7ba35842
Vanroy Julien <julienvanroy@yahoo.fr>
2021-04-07 16:14:16 +0200

    feat: show all end messages

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,10 +9,13 @@ import {tipsState} from "./tips/types";
 import {tipsReducer} from "./tips/reducer";
 import {knowsState} from "./knows/types";
 import {knowsReducer} from "./knows/reducer";
+import {messagesState} from "./messages/types";
+import {messagesReducer} from "./messages/reducer";
 
 export interface ApplicationState {
     user: userState;
     knows: knowsState;
+    messages: messagesState
     tips: tipsState;
     router: RouterState;
 }
@@ -21,6 +24,7 @@ export const createRootReducer = (history: History) =>
     combineReducers({
         user: userReducer,
         knows: knowsReducer,
+        messages: messagesReducer,
         tips: tipsReducer,
         router: connectRouter(history)
     });
---
+++ src/store/messages/types.ts
@@ -0,0 +1,33 @@
+export interface Message {
+  id?: number
+  key: string
+  title: string
+  description: string
+  position: number
+}
+
+export interface messagesState {
+  readonly loading: boolean;
+  readonly data: Message[];
+  readonly errors?: string;
+}
+
+export const FETCH_MESSAGES_REQUEST = "@@messages/FETCH_MESSAGES_REQUEST";
+export const FETCH_MESSAGES_SUCCESS = "@@messages/FETCH_MESSAGES_SUCCESS";
+export const FETCH_MESSAGES_ERROR = "@@messages/FETCH_MESSAGES_ERROR";
+
+interface FetchMessagesRequest {
+  type: typeof FETCH_MESSAGES_REQUEST
+}
+
+interface FetchMessagesSuccess {
+  type: typeof FETCH_MESSAGES_SUCCESS
+  payload: Message[]
+}
+
+interface FetchMessagesError {
+  type: typeof FETCH_MESSAGES_ERROR
+  payload: string
+}
+
+export type MessagesActionTypes = FetchMessagesRequest | FetchMessagesSuccess | FetchMessagesError;
