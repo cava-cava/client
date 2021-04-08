067ed46b464d8cb23735274ae857d5eba0a7cd30
Vanroy Julien <julienvanroy@yahoo.fr>
2021-04-08 22:42:30 +0200

    wip: rotation card

--- src/components/ThreeMeshes/Card.tsx
+++ src/components/ThreeMeshes/Card.tsx
@@ -1,29 +1,30 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef, useState } from "react";
 
-import { RoundedBox, MeshWobbleMaterial } from "@react-three/drei";
+import { RoundedBox } from "@react-three/drei";
 import { useSpring, a } from "react-spring/three";
 import { useFrame } from "@react-three/fiber";
 
 const ThreeCard = () => {
-    const mesh = useRef<THREE.Mesh>(null!); 
-    const [flipped, setFlipped] = useState(false)
+    const mesh = useRef<THREE.Mesh>(null!);
+    const [active, setActive] = useState(false);
 
     useFrame((state) => {
         // mesh.current.rotation.x = mesh.current.rotation.y += 0.01
 
     });
-  
 
-      const spring = useSpring({
-        rotation: flipped ? {x: 0, y: 0, z: 0} : {x: 0, y: 0, z: 0}
-      });
+    const { rotationY } = useSpring({
+        spring: active,
+        config: { mass: 5, tension: 400, friction: 50, precision: 0.0001 },
+        rotationY: active ? 0 : Math.PI
+    });
 
     return (
-      <a.mesh ref={mesh} onClick={() => setFlipped(!flipped)} rotation={spring.rotation} >
+      <a.mesh ref={mesh} onClick={() => setActive(!active)} rotation-y={rotationY} >
         <RoundedBox radius={0.1} args={[3, 5, 0.1]} attach="geometry" />
         <meshStandardMaterial attach="material" color="lightblue" />
       </a.mesh>
     );
   };
 
-export default ThreeCard 
+export default ThreeCard
--- src/pages/Labs.tsx
+++ src/pages/Labs.tsx
@@ -1,19 +1,15 @@
 import styles from "./Labs.module.scss";
-import { useRef, useEffect, useState } from "react";
 // three
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import OrbitControls from "../components/OrbitControls/OrbitControls";
-import { RoundedBox, MeshWobbleMaterial } from "@react-three/drei";
-import { useSpring, a } from "react-spring/three";
-import { BoxBufferGeometry } from "three";
 
 import ThreeCard from "../components/ThreeMeshes/Card";
 
 
 const Labs = () => {
-  return (
+    return (
     <div className={styles.Labs}>
-      <Canvas  shadowMap colorManagement camera={{ position: [0, 0, 5] }}>
+      <Canvas shadows camera={{ position: [0, 0, 5] }}>
         <ThreeCard />
         <OrbitControls />
       </Canvas>
