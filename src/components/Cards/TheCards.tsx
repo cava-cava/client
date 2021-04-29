ed0174437aa5a656f670f75bf09c8f9468bdb314
LeoGeneret <lpgeneret@gmail.com>
2021-04-29 10:13:38 +0200

    wip: integration card + dynamisation

--- src/components/Cards/TheCards.tsx
+++ src/components/Cards/TheCards.tsx
@@ -1,17 +1,24 @@
-import React, {FunctionComponent} from 'react';
-import styles from './TheCards.module.scss'
+import React, { FunctionComponent } from "react";
+import styles from "./TheCards.module.scss";
+
+import lottie from "./../../assets/anims/teste/data.json";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
+import cardWaouh from "./../../assets/png/carte_waouh.png";
 
 type TheCardsProps = {
-    Description: string
-}
+  Description: string;
+};
 
-const TheCards: FunctionComponent<TheCardsProps> = ({Description}) => {
-
-    return (
-        <div className={styles.TheCards}>
-            {Description}
-        </div>
-    )
-}
+const TheCards: FunctionComponent<TheCardsProps> = ({ Description }) => {
+  return (
+    <div className={styles.TheCards}>
+      <div className={styles.CardContainer}>
+        <img className={styles.Card} src={cardWaouh}></img>
+        <Player autoplay loop src={lottie} className={styles.anim} />
+        <div className={styles.description}>{Description}</div>
+      </div>
+    </div>
+  );
+};
 
 export default TheCards;
