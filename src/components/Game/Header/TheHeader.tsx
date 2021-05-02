814320356c6490efbc333758469d8a596fca1cf8
Vanroy Julien <julienvanroy@yahoo.fr>
2021-05-02 23:09:58 +0200

    fix: code diconnected users

--- src/components/Game/Header/TheHeader.tsx
+++ src/components/Game/Header/TheHeader.tsx
@@ -3,7 +3,6 @@ import UserHeader from "./UserHeader";
 import styles from  "./TheHeader.module.scss"
 import HelpHeader from "./HelpHeader";
 import SettingsHeader from "./SettingsHeader";
-import codeBorder from '../../../assets/svg/codeBorder.svg'
 import {User} from "../../../store/user/types";
 
 type TheHeaderProps = {
@@ -14,12 +13,10 @@ type TheHeaderProps = {
 const TheHeader: FunctionComponent<TheHeaderProps> = ({user, code}) =>
         <header className={styles.TheHeader}>
             <UserHeader user={user} roomId={code}/>
-            <div className={styles.codeContainer}>
-            <img className={styles.border} src={codeBorder}/>
-                <div className={styles.code}>{code}</div>
+            <div>
+                <HelpHeader />
+                <SettingsHeader />
             </div>
-            <HelpHeader />
-            <SettingsHeader />
         </header>
 
 export default TheHeader;
