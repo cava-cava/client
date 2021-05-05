22b842be6ae8802684229edbb4c1e01ffadb70eb
Vanroy Julien <julienvanroy@yahoo.fr>
2021-05-05 15:17:29 +0200

    feat: add slider in select avatar

--- src/components/Avatar/SelectAvatar.tsx
+++ src/components/Avatar/SelectAvatar.tsx
@@ -5,9 +5,10 @@ import {SET_AVATAR} from "../../store/user/types";
 
 type SelectAvatarProps = {
     color: string
+    avatarNumber: string
 }
 
-const SelectAvatar: FunctionComponent<SelectAvatarProps> = ({color}) => {
+const SelectAvatar: FunctionComponent<SelectAvatarProps> = ({color, avatarNumber}) => {
     const dispatch = useDispatch();
 
     const setAvatar = (value: string) => {
@@ -17,14 +18,14 @@ const SelectAvatar: FunctionComponent<SelectAvatarProps> = ({color}) => {
     return (
         <div className={styles.SelectAvatar}>
             <div>
-                <button onClick={() => setAvatar('1')}><img src={`/smiley/${color}/smiley_1.png`}/></button>
-                <button onClick={() => setAvatar('2')}><img src={`/smiley/${color}/smiley_2.png`}/></button>
-                <button onClick={() => setAvatar('3')}><img src={`/smiley/${color}/smiley_3.png`}/></button>
-                <button onClick={() => setAvatar('4')}><img src={`/smiley/${color}/smiley_4.png`}/></button>
+                <button style={{opacity: avatarNumber === '1' ? 0.5 : 1}} onClick={() => setAvatar('1')}><img src={`/smiley/${color}/smiley_1.png`}/></button>
+                <button style={{opacity: avatarNumber === '2' ? 0.5 : 1}} onClick={() => setAvatar('2')}><img src={`/smiley/${color}/smiley_2.png`}/></button>
+                <button style={{opacity: avatarNumber === '3' ? 0.5 : 1}} onClick={() => setAvatar('3')}><img src={`/smiley/${color}/smiley_3.png`}/></button>
+                <button style={{opacity: avatarNumber === '4' ? 0.5 : 1}} onClick={() => setAvatar('4')}><img src={`/smiley/${color}/smiley_4.png`}/></button>
             </div>
             <div>
-                <button onClick={() => setAvatar('5')}><img src={`/smiley/${color}/smiley_5.png`}/></button>
-                <button onClick={() => setAvatar('6')}><img src={`/smiley/${color}/smiley_6.png`}/></button>
+                <button style={{opacity: avatarNumber === '5' ? 0.5 : 1}} onClick={() => setAvatar('5')}><img src={`/smiley/${color}/smiley_5.png`}/></button>
+                <button style={{opacity: avatarNumber === '6' ? 0.5 : 1}} onClick={() => setAvatar('6')}><img src={`/smiley/${color}/smiley_6.png`}/></button>
             </div>
         </div>
     )
--- src/pages/Setup.tsx
+++ src/pages/Setup.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import {useSelector} from "react-redux";
 import {ApplicationState} from "../store";
 import styles from './Setup.module.scss'
-import AvatarHeader from "../components/Avatar/AvatarHeader";
 import SelectAvatar from "../components/Avatar/SelectAvatar";
 import FormUsername from "../components/Form/FormUsername";
+import SliderAvatar from "../components/Avatar/SliderAvatar";
 
 const Setup = () => {
     const user = useSelector((state: ApplicationState) => state.user.data);
@@ -13,8 +13,8 @@ const Setup = () => {
         <div className={styles.Setup}>
             <div>
                 <div>
-                    <AvatarHeader color={"black"} avatarNumber={user.avatar} />
-                    <SelectAvatar color={"black"} />
+                    <SliderAvatar color={'black'} avatarNumber={user.avatar}/>
+                    <SelectAvatar color={"black"} avatarNumber={user.avatar}/>
                 </div>
                <FormUsername name={user.name} showSubmit={true}/>
             </div>
