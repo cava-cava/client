7199014b4b92b8e3beed7107d8cfd92d600834ec
Vanroy Julien <julienvanroy@yahoo.fr>
2021-05-22 20:51:04 +0200

    fix: orthographe

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
     return (
         <div className={styles.Home}>
             <img src={homeImg} alt="ça va ça va"/>
-            <p>Le jeu complètement déjantaient qui
+            <p>Le jeu complètement déjanté qui
                 vous fera relativiser sur le bonheur !</p>
             <Link to="/setup">Jouer !</Link>
         </div>
