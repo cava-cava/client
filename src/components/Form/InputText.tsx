f94c76707065b3ab62a26e111cedcd7ecbcc162f
Vanroy Julien <julienvanroy@yahoo.fr>
2021-06-04 23:14:52 +0200

    style: try to fix autocomplete input text

--- src/components/Form/InputText.tsx
+++ src/components/Form/InputText.tsx
@@ -5,18 +5,31 @@ type InputTextProps = {
     id?: string,
     name: string,
     placeholder?: string,
+    autoComplete?: string,
     maxLength?: number,
     value?: string,
     setValue?: (value: string) => void,
     hasError?: boolean
 }
 
-const InputText: FunctionComponent<InputTextProps> = ({id, name, maxLength, placeholder, value, setValue, hasError}) => {
+const InputText: FunctionComponent<InputTextProps> = ({
+                                                          id,
+                                                          name,
+                                                          maxLength,
+                                                          placeholder,
+                                                          autoComplete = "off",
+                                                          value,
+                                                          setValue,
+                                                          hasError
+                                                      }) => {
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        if(setValue) setValue(event.target.value);
+        if (setValue) setValue(event.target.value);
     }
 
-    return (<input type="text" className={`${styles.InputText} ${hasError && styles.InputTextError}`} id={id} name={name} maxLength={maxLength} placeholder={placeholder} value={value} onChange={handleChange} />)
+    return (
+        <input type="text" className={`${styles.InputText} ${hasError && styles.InputTextError}`} id={id} name={name}
+               maxLength={maxLength} placeholder={placeholder} autoComplete={autoComplete} value={value}
+               onChange={handleChange}/>)
 }
 
 export default InputText;
