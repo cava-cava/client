1093bf33feceb6e1b8128758db5bd04d4df76e35
Vanroy Julien <julienvanroy@yahoo.fr>
2021-06-07 11:51:56 +0200

    fix(Tips): wording

--- src/components/Tips/ListTips.tsx
+++ src/components/Tips/ListTips.tsx
@@ -10,7 +10,7 @@ type ListTipsProps = {
 const ListTips: FunctionComponent<ListTipsProps> = ({tips}) => {
     return (
         <div className={styles.ListTips}>
-            {tips.length === 0 ? <div>Loading...</div> : tips.map((tip:Tip, index:number) => <MessageTips key={index} index={index} message={tip.message} date={tip.published_at}/>)}
+            {tips.length === 0 ? <div>Chargement...</div> : tips.map((tip:Tip, index:number) => <MessageTips key={index} index={index} message={tip.message} date={tip.published_at}/>)}
         </div>)
 }
 
