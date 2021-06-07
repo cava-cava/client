d65469e52cd0d96e2736da8c1f70f74bdd5bafaa
Vanroy Julien <julienvanroy@yahoo.fr>
2021-06-07 10:37:58 +0200

    fix: Audio for mobile

--- src/hooks/useAudio.tsx
+++ src/hooks/useAudio.tsx
@@ -1,4 +1,4 @@
-import {Howl} from "howler";
+import {Howl, Howler} from "howler";
 import {useEffect, useState} from "react";
 
 const useAudio = (src: string, autoplay: boolean, loop: boolean) => {
@@ -6,10 +6,16 @@ const useAudio = (src: string, autoplay: boolean, loop: boolean) => {
     const [source, setSource] = useState<string>(src)
 
     useEffect(() => {
+        Howler.autoUnlock = false;
         setAudio(new Howl({
             src: [source],
             autoplay: autoplay,
-            loop: loop
+            loop: loop,
+            onplayerror: function() {
+                audio?.once('unlock', function() {
+                    audio?.play();
+                });
+            }
         }))
         return () => {
             audio?.once( 'fade', () => { audio?.stop() })
